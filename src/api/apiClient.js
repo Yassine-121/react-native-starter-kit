eb0f7a777f08c90de5a89a0516d7ab1556f5d3bd
Yassine <y.lyazighi@alvariumgroup.com>
2022-12-07 00:00:51 +0100

    apiClient axios adjusment

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,12 +1,15 @@
 import axios from "axios";
-import {getData} from "../utils/localStorage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const axiosClient = axios.create({
-    baseURL: 'http://192.168.11.109:8000/api',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer '+ getData('@user_token')
-    }
+    baseURL: 'http://192.168.11.109:8000/api'
 });
+
+axiosClient.interceptors.request.use(async function (config) {
+    const token = await AsyncStorage.getItem('user_token')
+    config.headers['Authorization'] = token ? 'Bearer '+token : 'Bearer ';
+    config.headers['Content-Type'] = 'application/json';
+    return config
+})
 
 export default axiosClient;
--- src/navigations/appNavigation.js
+++ src/navigations/appNavigation.js
@@ -2,6 +2,7 @@ import React, {useContext} from "react";
 import {createDrawerNavigator, DrawerContentScrollView, DrawerItem, DrawerItemList} from "@react-navigation/drawer";
 import DashboardScreen from "../pages/dashboard";
 import { AuthContext } from "../context/authContext";
+import Toast from "react-native-toast-message";
 
 const Drawer = createDrawerNavigator();
 
@@ -17,9 +18,12 @@ function CustomDrawerContent(props) {
 
 const AppNavigation = () => {
     return (
-        <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
-            <Drawer.Screen name={"Dashboard"} component={DashboardScreen} />
-        </Drawer.Navigator>
+        <>
+            <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
+                <Drawer.Screen name={"Dashboard"} component={DashboardScreen} />
+            </Drawer.Navigator>
+            <Toast />
+        </>
     );
 }
 
